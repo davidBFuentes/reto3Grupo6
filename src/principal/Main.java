e00078c9a2b68037396117c755f5e4f21e08ba65
davidbf <david.barriofu@elorrieta-errekamari.com>
2020-02-07 16:15:48 +0100

    Código comentado en main

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -8,8 +8,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Se crea la primera ventana
+		
 		Ventana01Bienvenida ventana01 = new Ventana01Bienvenida();
+		
+		// Se crea el controlador de la primera ventana y se le mandan sus elementos
+		
 		Controlador01Bienvenida controladorBienvenida = new Controlador01Bienvenida(ventana01);
+		
+		// Se muestra el frame de la primera ventana llamando al método del controlador
+		
 		controladorBienvenida.mMostrarFrame();
 		
 	}
